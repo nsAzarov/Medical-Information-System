5049d2cf6c221b642b1a0ee2475ae0ae7ae6c5c0
nsAzarov <nsazarov99@gmail.com>
2019-11-22 23:50:04 +0300

    connect to mongoDB

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
 
 app.use(cors());
+
+/*app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());*/
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static('client/build'));
